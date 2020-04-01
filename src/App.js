2e21177d76d5b985ac1b13124579255d2793ffd2
Steve Doty <stevedoty16@gmail.com>
2020-03-31 19:21:12 -0700

    handleChange for SearchBox

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ class App extends Component {
             monsters: [],
             searchField: '',
         }
+
     }
 
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => this.setState({ monsters: users }))
+    }
+
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value })
     }
 
     render() {
@@ -31,7 +36,7 @@ class App extends Component {
 
             <SearchBox
                 placeholder='search monsters'
-                handleChange={e => this.setState({ searchField: e.target.value })}
+                handleChange={ this.handleChange }
             />
 
             <CardList monsters={ filteredMonsters } />
