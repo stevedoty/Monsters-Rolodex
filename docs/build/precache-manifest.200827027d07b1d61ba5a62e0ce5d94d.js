7bcdd93469f6a833cc3bfcd6e3216a201d8b4444
Steve Doty <stevedoty16@gmail.com>
2020-08-01 11:05:38 -0700

    add homepage

---
+++ docs/build/precache-manifest.200827027d07b1d61ba5a62e0ce5d94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff8b8e01074e63ee7a7d723e134ea4d",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1dc170214c8fcf71e602",
+    "url": "/Monsters-Rolodex/static/css/main.36a98575.chunk.css"
+  },
+  {
+    "revision": "13b7fe759aeb0136376e",
+    "url": "/Monsters-Rolodex/static/js/2.2828ef60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.2828ef60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc170214c8fcf71e602",
+    "url": "/Monsters-Rolodex/static/js/main.baf42767.chunk.js"
+  },
+  {
+    "revision": "9cf362cb55acc30282a1",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.8ec4fb89.js"
+  }
+]);
