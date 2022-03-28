a4dd5a64e85d40dfae33c1545d0f6ea505687996
Steve Doty <stevedoty16@gmail.com>
2022-03-28 09:49:07 -0700

    -

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
             searchField: '',
         }
     }
-
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
